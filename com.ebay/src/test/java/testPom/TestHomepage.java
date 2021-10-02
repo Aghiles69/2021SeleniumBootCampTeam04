b58aaa20c8398d6755ab302fff85b695d697ffee
= <=>
2021-10-02 19:26:02 -0400

    2nd commit

---
+++ com.ebay/src/test/java/testPom/TestHomepage.java
@@ -0,0 +1,29 @@
+package testPom;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pom.Homepage;
+import testBase.TestBase;
+
+public class TestHomepage extends TestBase {
+
+    Homepage homepage;
+
+    @Test(enabled = true)
+    public void testDoSearch() throws Exception {
+        homepage = getHomepage();
+
+        homepage.doSearch("Playstation 5", "Video Games & Consoles");
+//        String expTitle = "playstation 5 in Video Games and Consoles | eBay";
+
+//        waitForElementToBeVisible(homepage.searchResultPageElement);
+//        scrollJS(1000);
+//        Thread.sleep(1000);
+//        scrollJS(-500);
+//        Thread.sleep(1000);
+//        scrollJS(6000);
+//        Thread.sleep(1000);
+
+//        Assert.assertEquals(driver.getTitle(), expTitle);
+    }
+}
