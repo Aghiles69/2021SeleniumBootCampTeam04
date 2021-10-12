a9d46b31551e28504577ab00689d6297e8fbaae6
= <=>
2021-10-12 17:49:36 -0400

    added more test cases

--- com.airbnb/src/main/java/testBase/HomePage.java
+++ com.airbnb/src/main/java/testBase/HomePage.java
@@ -24,6 +24,31 @@ public class HomePage extends BaseClass {
     @FindBy(xpath = "//*[@id=\"search-tabpanel\"]/div/div[3]/div[1]/div/div/div[2]")
     public WebElement checkInDate;
 
+    @FindBy(xpath = "//*[@id=\"search-tabpanel\"]/div/div[3]/div[3]/div/div/div[2]")
+    public WebElement checkOutDate;
+
+    @FindBy(xpath = "//*[@id=\"search-tabpanel\"]/div/div[5]/div[1]/div/div[2]")
+    public WebElement guestDropDown;
+
+    @FindBy(xpath = "//*[@id=\"stepper-adults\"]/button[2]/span/svg/path")
+    public WebElement add1Adult;
+
+    @FindBy(xpath = "//*[@id=\"stepper-adults\"]/button[1]/span/svg")
+    public WebElement remove1Adult;
+
+    @FindBy(xpath = "//*[@id=\"stepper-children\"]/button[2]/span/svg")
+    public WebElement add1Children;
+
+    @FindBy(xpath = "//*[@id=\"stepper-children\"]/button[1]/span/svg/path")
+    public WebElement remove1Children;
+
+    @FindBy(xpath = "//*[@id=\"stepper-infants\"]/button[2]/span/svg")
+    public WebElement add1Infants;
+
+    @FindBy(xpath = "//*[@id=\"stepper-infants\"]/button[1]")
+    public WebElement remove1Infants;
+
+
     public void doSearchWithOnlyLocation(String keys) {
         waitForElementToBeVisible(locationInputBox);
         clicklocationInputBox();
@@ -43,8 +68,51 @@ public class HomePage extends BaseClass {
         sendKeysTocheckInDate(keys);
         clicksearchButton();
     }
-
-
+    public void doSearchWithCheckOutdate(String keys) {
+        waitForElementToBeVisible(locationInputBox);
+        clicklocationInputBox();
+        sendKeysTolocationInputBox(keys);
+        sendKeysTocheckOutDate(keys);
+        clicksearchButton();
+    }
+    public void doSearchWithCheckinAndOut(String keys) {
+        waitForElementToBeVisible(locationInputBox);
+        clicklocationInputBox();
+        sendKeysTolocationInputBox(keys);
+        sendKeysTocheckInDate(keys);
+        sendKeysTocheckOutDate(keys);
+        clicksearchButton();
+    }
+    public void doSearchWithOnlyAddingAdults() {
+        clickGuestDropDown();
+        clickadd1Adult();
+        clicksearchButton();
+    }
+    public void doSearchWithOnlyRemovingAdults() {
+        clickGuestDropDown();
+        clickremove1Adult();
+        clicksearchButton();
+    }
+    public void doSearchWithOnlyAddingChildren() {
+        clickGuestDropDown();
+        clickadd1Children();
+        clicksearchButton();
+    }
+    public void doSearchWithOnlyRemovingChildren() {
+        clickGuestDropDown();
+        clickremove1Children();
+        clicksearchButton();
+    }
+    public void doSearchWithOnlyAddingInfants() {
+        clickGuestDropDown();
+        clickadd1Infants();
+        clicksearchButton();
+    }
+    public void doSearchWithOnlyRemovingInfants() {
+        clickGuestDropDown();
+        clickremove1Infants();
+        clicksearchButton();
+    }
 
     private void sendKeysTolocationInputBox(String keys) {
         sendKeysToElement(locationInputBox, keys);}
@@ -52,4 +120,16 @@ public class HomePage extends BaseClass {
     public void clicklocationInputBox(){clickJScript(locationInputBox);}
     private void sendKeysTocheckInDate(String keys) {
         sendKeysToElement(checkInDate, keys);}
+    private void sendKeysTocheckOutDate(String keys) {
+        sendKeysToElement(checkOutDate, keys);}
+    public void clickGuestDropDown(){clickJScript(guestDropDown);}
+    public void clickadd1Adult(){clickJScript(add1Adult);}
+    public void clickremove1Adult(){clickJScript(remove1Adult);}
+    public void clickadd1Children(){clickJScript(add1Children);}
+    public void clickremove1Children(){clickJScript(remove1Children);}
+    public void clickadd1Infants(){clickJScript(add1Infants);}
+    public void clickremove1Infants(){clickJScript(remove1Infants);}
+
+
+
 }
--- com.airbnb/src/test/java/testPom/TestHomePage.java
+++ com.airbnb/src/test/java/testPom/TestHomePage.java
@@ -24,5 +24,49 @@ public class TestHomePage extends BaseClass {
         String expectedText = "Stays in Bali";
         Assert.assertEquals(actualText,expectedText);
     }
-
+    @Test(description = "TC003",enabled = true)
+    public void doSearchWithCheckInDate(){
+        homePage = new HomePage();
+        homePage.doSearchWithCheckInDate("1");
+    }
+    @Test(description = "TC004",enabled = true)
+    public void doSearchWithCheckOutdate(){
+        homePage = new HomePage();
+        homePage.doSearchWithCheckOutdate("1");
+    }
+    @Test(description = "TC005",enabled = true)
+    public void doSearchWithCheckinAndOut(){
+        homePage = new HomePage();
+        homePage.doSearchWithCheckinAndOut("1");
+    }
+    @Test(description = "TC006",enabled = true)
+    public void doSearchWithOnlyAddingAdults(){
+        homePage = new HomePage();
+        homePage.doSearchWithOnlyAddingAdults();
+    }
+    @Test(description = "TC007",enabled = true)
+    public void doSearchWithOnlyRemovingAdults(){
+        homePage = new HomePage();
+        homePage.doSearchWithOnlyRemovingAdults();
+    }
+    @Test(description = "TC008",enabled = true)
+    public void doSearchWithOnlyAddingChildren(){
+        homePage = new HomePage();
+        homePage.doSearchWithOnlyAddingChildren();
+    }
+    @Test(description = "TC009",enabled = true)
+    public void doSearchWithOnlyRemovingChildren(){
+        homePage = new HomePage();
+        homePage.doSearchWithOnlyRemovingChildren();
+    }
+    @Test(description = "TC010",enabled = true)
+    public void doSearchWithOnlyAddingInfants(){
+        homePage = new HomePage();
+        homePage.doSearchWithOnlyAddingInfants();
+    }
+    @Test(description = "TC011",enabled = true)
+    public void doSearchWithOnlyRemovingInfants(){
+        homePage = new HomePage();
+        homePage.doSearchWithOnlyRemovingInfants();
+    }
 }
